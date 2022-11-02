3bbf22f2d2418894829ad1d82e7bb09bc7f1d77a
fwhyn <fwhynseventh@gmail.com>
2022-11-02 21:01:16 +0700

    databinding example

---
+++ app/src/main/java/com/fwhyn/myapplication/databindingsample/ClickHandler.kt
@@ -0,0 +1,7 @@
+package com.fwhyn.myapplication.databindingsample
+
+import android.view.View
+
+interface ClickHandler {
+    fun onClick(view: View)
+}
--- app/src/main/java/com/fwhyn/myapplication/databindingsample/UserActivity.kt
+++ app/src/main/java/com/fwhyn/myapplication/databindingsample/UserActivity.kt
@@ -4,20 +4,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.fwhyn.myapplication.databinding.ActivityUserBinding
+import com.google.android.material.snackbar.Snackbar
 
-class UserActivity : AppCompatActivity() {
+class UserActivity : AppCompatActivity(), ClickHandler {
+    private lateinit var parentView: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding: ActivityUserBinding = ActivityUserBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        parentView = binding.root
+        setContentView(parentView)
 
         val userTest = User("Test", "User")
-
         binding.user = userTest
+        binding.presenter = Presenter()
+    }
 
-//        binding.buttonTest.setOnClickListener {
-//            userTest.firstName = "aaa"
-//        }
+    override fun onClick(view: View) {
+        Snackbar.make(parentView, "tset", Snackbar.LENGTH_SHORT)
+            .setAction("close", null)
+            .show()
     }
 }

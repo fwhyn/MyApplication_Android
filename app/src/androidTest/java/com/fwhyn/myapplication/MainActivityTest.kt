4601ddabfd9f500e6cd27aae8d9104c2c8945448
fwhyn <fwhynseventh@gmail.com>
2022-11-09 18:59:45 +0700

    add test

--- app/src/androidTest/java/com/fwhyn/myapplication/MainActivityTest.kt
+++ app/src/androidTest/java/com/fwhyn/myapplication/MainActivityTest.kt
@@ -8,15 +8,15 @@ import org.junit.Rule
 import org.junit.Test
 
 class MyActivityTest {
-    @Rule
-    @JvmField
-    val composeTestRule = createAndroidComposeRule<MainActivity>()
-
-    @Test
-    fun testGreeting() {
-        val greeting = InstrumentationRegistry.getInstrumentation()
-            .targetContext.resources.getString(R.string.greeting)
-
-        composeTestRule.onNodeWithText(greeting).assertIsDisplayed()
-    }
+//    @Rule
+//    @JvmField
+//    val composeTestRule = createAndroidComposeRule<MainActivity>()
+//
+//    @Test
+//    fun testGreeting() {
+//        val greeting = InstrumentationRegistry.getInstrumentation()
+//            .targetContext.resources.getString(R.string.greeting)
+//
+//        composeTestRule.onNodeWithText(greeting).assertIsDisplayed()
+//    }
 }
--- app/src/main/java/com/fwhyn/myapplication/javatest/CustomSetKotlin.kt
+++ app/src/main/java/com/fwhyn/myapplication/javatest/CustomSetKotlin.kt
@@ -1,8 +1,34 @@
 package com.fwhyn.myapplication.javatest
 
 class CustomSetKotlin {
-    private var objectKotlin = arrayOfNulls<Any>(4)
-    fun add(value: Any?) {
+
+    private var objectKotlin = arrayOf<Any?>()
+    fun customAdd(value: Any?) {
         objectKotlin[0] = value
+    }
+
+    fun isSame(a: Int, b: Int): Boolean {
+        return (a == b)
+    }
+
+    fun isSame(a: String, b: String): Boolean {
+        return (a == b)
+    }
+
+    fun isSame(): Boolean {
+        val a = "Test"
+        val b = a
+
+        return b.equals(a)
+    }
+
+    fun test() {
+        val a = arrayListOf<String> ("a", "b")
+        a.add("b")
+
+        val b = listOf<String> ("a", "b")
+
+        val c = mutableListOf<String> ("a", "b")
+        c.add("asgd")
     }
 }

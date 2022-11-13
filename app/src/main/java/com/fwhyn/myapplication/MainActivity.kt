62f5e8fd34a6b16d1a8f3ee5a9c0bb5b8edc4a37
fwhyn <fwhynseventh@gmail.com>
2022-11-13 16:50:29 +0700

    mockito test
    complete game

--- app/src/main/java/com/fwhyn/myapplication/MainActivity.kt
+++ app/src/main/java/com/fwhyn/myapplication/MainActivity.kt
@@ -3,18 +3,8 @@ package com.fwhyn.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import com.fwhyn.myapplication.databindingsample.UserActivity
+import com.fwhyn.myapplication.cocktails.CocktailsGameActivity
 import kotlinx.coroutines.Job
 
 class MainActivity : AppCompatActivity() {
@@ -25,7 +15,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.button_main).setOnClickListener {
-            startActivity(Intent(this, UserActivity::class.java))
+//            startActivity(Intent(this, UserActivity::class.java))
+            startActivity(Intent(this, CocktailsGameActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/fwhyn/myapplication/cocktails/common/repository/CocktailsRepositoryImpl.kt
+++ app/src/main/java/com/fwhyn/myapplication/cocktails/common/repository/CocktailsRepositoryImpl.kt
@@ -3,6 +3,10 @@ package com.fwhyn.myapplication.cocktails.common.repository
 import android.content.SharedPreferences
 import com.fwhyn.myapplication.cocktails.common.network.Cocktail
 import com.fwhyn.myapplication.cocktails.common.network.CocktailsApi
+import com.fwhyn.myapplication.cocktails.common.network.CocktailsContainer
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 private const val HIGH_SCORE_KEY = "HIGH_SCORE_KEY"
 
@@ -11,9 +15,31 @@ class CocktailsRepositoryImpl(
     private val sharedPreferences: SharedPreferences
 ) : CocktailsRepository {
 
+    private var getAlcoholicCall: Call<CocktailsContainer>? = null
+
     override fun getAlcoholic(callback: RepositoryCallback<List<Cocktail>, String>) {
-        TODO("Not yet implemented")
+        getAlcoholicCall?.cancel()
+        getAlcoholicCall = api.getAlcoholic()
+        getAlcoholicCall?.enqueue(wrapCallback(callback))
     }
+
+    private fun wrapCallback(callback: RepositoryCallback<List<Cocktail>, String>) =
+        object : Callback<CocktailsContainer> {
+            override fun onResponse(call: Call<CocktailsContainer>, response: Response<CocktailsContainer>) {
+                if (response.isSuccessful) {
+                    val cocktailsContainer = response.body()
+                    if (cocktailsContainer != null) {
+                        callback.onSuccess(cocktailsContainer.drinks ?: emptyList())
+                        return
+                    }
+                }
+                callback.onError("Couldn't get cocktails")
+            }
+
+            override fun onFailure(call: Call<CocktailsContainer>, t: Throwable) {
+                callback.onError("Couldn't get cocktails")
+            }
+        }
 
     override fun saveHighScore(score: Int) {
         val highScore = getHighScore()

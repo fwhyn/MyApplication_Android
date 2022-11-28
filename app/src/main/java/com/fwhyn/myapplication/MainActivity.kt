5ca1d06715fc143f3ce34bcc409408d835d8e82a
fwhyn <fwhynseventh@gmail.com>
2022-11-28 21:08:16 +0700

    wishlist
    try run module

--- app/src/main/java/com/fwhyn/myapplication/MainActivity.kt
+++ app/src/main/java/com/fwhyn/myapplication/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.fwhyn.wishlist.app.WishlistSplashActivity
+//import com.fwhyn.wishlist.app.WishlistSplashActivity
 import kotlinx.coroutines.Job
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button_main).setOnClickListener {
 //            startActivity(Intent(this, UserActivity::class.java))
 //            startActivity(Intent(this, CocktailsGameActivity::class.java))
-            startActivity(Intent(this, WishlistSplashActivity::class.java))
+//            startActivity(Intent(this, WishlistSplashActivity::class.java))
         }
     }
 }

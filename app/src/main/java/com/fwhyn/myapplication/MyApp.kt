6f0f055694ffbe3e3845c60928b31cd7082ec0fc
fwhyn <fwhynseventh@gmail.com>
2022-12-18 08:52:31 +0700

    #di
    move koin

--- app/src/main/java/com/fwhyn/myapplication/MyApp.kt
+++ app/src/main/java/com/fwhyn/myapplication/MyApp.kt
@@ -3,8 +3,19 @@ package com.fwhyn.myapplication
 import android.app.Application
 import com.fwhyn.myapplication.di.AppComponent
 import com.fwhyn.myapplication.di.DaggerAppComponent
+import com.fwhyn.wishlist.app.wishlistAppModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext
+import org.koin.core.context.startKoin
 
 open class MyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin()
+    }
+
     // Instance of the AppComponent that will be used by all the Activities in the project
     val appComponent: AppComponent by lazy {
         initializeComponent()
@@ -14,5 +25,19 @@ open class MyApp : Application() {
         // Creates an instance of AppComponent using its Factory constructor
         // We pass the applicationContext that will be used as Context in the graph
         return DaggerAppComponent.create()
+    }
+
+    // start koin
+    fun startKoin() {
+        if (GlobalContext.getOrNull() == null) {
+            startKoin {
+                // declare Android context
+                androidContext(this@MyApp)
+                // declare used modules
+                modules(
+                    wishlistAppModule, //wishlist module
+                )
+            }
+        }
     }
 }
--- wishlist/src/main/java/com/fwhyn/wishlist/app/KoinModules.kt
+++ wishlist/src/main/java/com/fwhyn/wishlist/app/KoinModules.kt
@@ -39,7 +39,7 @@ import com.fwhyn.wishlist.persistance.WishlistDaoImpl
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val appModule = module {
+val wishlistAppModule = module {
 
     single<Repository> { RepositoryImpl(get()) }
 

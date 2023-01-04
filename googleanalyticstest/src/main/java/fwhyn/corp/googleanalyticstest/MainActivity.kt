afbc52296971d0cf6a3b99c1105e1f1d5508f6f6
Yana Wahyuna <yana.wahyuna@epson.co.id>
2023-01-04 15:17:26 +0700

    google analytics check
    rebase from develop

---
+++ googleanalyticstest/src/main/java/fwhyn/corp/googleanalyticstest/MainActivity.kt
@@ -0,0 +1,11 @@
+package fwhyn.corp.googleanalyticstest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

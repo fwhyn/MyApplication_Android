e13a34e0b2f52249d9ae9101c25ae82e83d738d2
Yana Wahyuna <yana.wahyuna@epson.co.id>
2023-01-05 17:46:33 +0700

    ga4 check
    try more event

--- ga4test/src/main/java/fwhyn/corp/ga4test/Ga4Activity.kt
+++ ga4test/src/main/java/fwhyn/corp/ga4test/Ga4Activity.kt
@@ -3,6 +3,7 @@ package fwhyn.corp.ga4test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
@@ -27,6 +28,26 @@ class Ga4Activity : AppCompatActivity() {
                 param(FirebaseAnalytics.Param.ITEM_NAME, "Test GA4")
                 param(FirebaseAnalytics.Param.CONTENT_TYPE, "image")
             }
+        }
+
+        findViewById<Button>(R.id.button1).setOnClickListener {
+            firebaseAnalytics.logEvent("share_image") {
+                param("image_name", "name")
+                param("full_text", "text")
+            }
+        }
+
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            val parameters = Bundle().apply {
+                this.putString("level_name", "Caverns01")
+                this.putInt("level_difficulty", 4)
+            }
+
+            firebaseAnalytics.setDefaultEventParameters(parameters)
+        }
+
+        findViewById<Button>(R.id.button3).setOnClickListener {
+            firebaseAnalytics.setDefaultEventParameters(null)
         }
     }
 }

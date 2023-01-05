bb397d8c55266a1e090411bb5b09f1f543d25d15
Yana Wahyuna <yana.wahyuna@epson.co.id>
2023-01-05 13:38:33 +0700

    ga4 check
    add module

---
+++ ga4test/src/main/java/fwhyn/corp/ga4test/Ga4Activity.kt
@@ -0,0 +1,11 @@
+package fwhyn.corp.ga4test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ga4Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ga4)
+    }
+}

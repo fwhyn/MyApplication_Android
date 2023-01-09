8aee5e9a439703b25665ec9f9cad9dc370b0886f
Yana Wahyuna <yana.wahyuna@epson.co.id>
2023-01-09 12:49:15 +0700

    google analytics
    added ga4

--- googleanalyticstest/src/main/java/fwhyn/corp/googleanalyticstest/GoogleAnalyticsActivity.kt
+++ googleanalyticstest/src/main/java/fwhyn/corp/googleanalyticstest/GoogleAnalyticsActivity.kt
@@ -2,15 +2,26 @@ package fwhyn.corp.googleanalyticstest
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.analytics.HitBuilders
 import com.google.android.gms.analytics.ecommerce.Product
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class GoogleAnalyticsActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_google_analytics)
+
+        // Obtain the FirebaseAnalytics instance.
+        firebaseAnalytics = Firebase.analytics
 
         // Obtain the shared Tracker instance.
         val app = application as AnalyticsApplication
@@ -19,12 +30,13 @@ class MainActivity : AppCompatActivity() {
         val name = "MainActivity"
         Log.d("fwhyn_test", "Setting screen name: $name")
         with(mTracker!!) {
-            setScreenName("Screen~$name")
+            setScreenName("Screen-$name")
             setAppName("My Application by fwhyn")
             send(HitBuilders.ScreenViewBuilder().build())
         }
-        findViewById<TextView>(R.id.text_view).setOnClickListener {
-            Log.d("fwhyn_test", "send tracker")
+
+        findViewById<Button>(R.id.ua).setOnClickListener {
+            Log.d("fwhyn_test", "send ua")
 
             val product = Product()
             with(product) {
@@ -46,6 +58,15 @@ class MainActivity : AppCompatActivity() {
                     .setCustomDimension(2, dimensionValue2)
                     .build()
             )
+        }
+
+        findViewById<Button>(R.id.ga4).setOnClickListener {
+            Log.d("fwhyn_test", "send ga4")
+
+            firebaseAnalytics.logEvent("test_event") {
+                param("string_param", "test")
+                param("integer_param", 99)
+            }
         }
     }
 }

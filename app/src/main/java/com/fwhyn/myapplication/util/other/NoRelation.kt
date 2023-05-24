149642127badee7b20a7d027a6975f2d29299209
Yana Wahyuna <yana.wahyuna@epson.co.id>
2023-05-24 08:52:02 +0700

    Updated material

--- app/src/main/java/com/fwhyn/myapplication/util/other/NoRelation.kt
+++ app/src/main/java/com/fwhyn/myapplication/util/other/NoRelation.kt
@@ -17,4 +17,47 @@ class NoRelation {
         }
         return true
     }
+
+    /**
+     * res/raw/case_study1.pdf
+     */
+
+    /**
+     * Case Study 1
+     *
+     * Question 1
+     * Info:
+     *      - cannot select preferred time
+     *      - only queue
+     *      - 1 doctor 3min/consultation
+     *      - 5 patients queue, not started yet
+     *      - total waiting times = 3 * 5 = 15min
+     *      - 6th patient queues (John)
+     *
+     * Question:
+     *      - When count down time should start?
+     *        a. The moment John queues
+     *        b. The moment doctor starts seeing 1th patient?
+     *
+     * Answer:
+     *      b. The moment doctor starts seeing 1th patient?
+     * Explanation:
+     *      - doctor has consultation 3 minutes means that when he starts seeing patient until
+     *      consultation finished (range 0 - 3min)
+     *      - before doctor starts seeing patient, the time will be undefined (can be infinite
+     *      number of time)
+     *
+     * Question 1
+     *
+     */
+
+    /**
+     *
+     *
+     */
+
+
+    fun case1() {
+
+    }
 }

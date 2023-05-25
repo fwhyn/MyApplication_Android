5f93184aa6257661e34ae5ea20dcbf7423d91b1e
Yana Wahyuna <yana.wahyuna@epson.co.id>
2023-05-25 14:14:05 +0700

    Updated material

--- app/src/main/java/com/fwhyn/myapplication/util/other/NoRelation.kt
+++ app/src/main/java/com/fwhyn/myapplication/util/other/NoRelation.kt
@@ -111,27 +111,20 @@ class NoRelation {
      */
 
 
-    fun getConsultationTime(doctors: List<Doctor>, patient: Int): Int? {
+    fun getConsultationTimeOrNull(doctors: List<Doctor>, patient: Int): Int? {
         // make sure that do not edit original source of doctor
         val copyOfDoctors = getCopyOfDoctors(doctors)
 
         var availableDoctor: Doctor? = null
-        for (i in 1..patient) {
-            availableDoctor = getDoctorWithMinTimeStepIncrement(copyOfDoctors)
-            val minTimeStep: Int = availableDoctor.timeStep
+        if (copyOfDoctors.isNotEmpty()) {
+            for (i in 1..patient) {
+                availableDoctor = getAvailableDoctor(copyOfDoctors)
 
-            val duplicatedDoctorTimeStepList =
-                getDuplicatedDoctorTimeStep(minTimeStep, copyOfDoctors)
-            if (duplicatedDoctorTimeStepList.size > 1) {
-                availableDoctor = getDoctorWithMinConsultationTime(duplicatedDoctorTimeStepList)
+                incrementDoctorTimeStep(availableDoctor)
             }
-
-            incrementDoctorTimeStep(availableDoctor)
         }
 
-        val curentTimeStep: Int? = availableDoctor?.timeStep
-
-        return curentTimeStep
+        return availableDoctor?.timeStep
     }
 
     private fun getCopyOfDoctors(doctorsToCopy: List<Doctor>): List<Doctor> {
@@ -143,24 +136,36 @@ class NoRelation {
         return copiedArray
     }
 
-    private fun getDuplicatedDoctorTimeStep(timeStep: Int, doctors: List<Doctor>): List<Doctor> {
-        val duplicatedList = ArrayList<Doctor>()
+    private fun getAvailableDoctor(doctors: List<Doctor>): Doctor {
+        val timeStep = getMinTimeStep(doctors)
+        val doctorsWithMinTimeStep = getDoctorsWithMinTimeStep(timeStep, doctors)
 
-        doctors.map {
-            if (timeStep == it.timeStep) {
-                duplicatedList.add(it)
-            }
-        }
-
-        return duplicatedList
+        return getDoctorWithMinConsultationTime(doctorsWithMinTimeStep)
     }
 
-    private fun getDoctorWithMinConsultationTime(doctors: List<Doctor>): Doctor {
-        return doctors.minBy { it.consultationTimeMinute }
+    private fun getMinTimeStep(doctors: List<Doctor>): Int {
+        val doctor = getDoctorWithMinTimeStepIncrement(doctors)
+
+        return doctor.timeStep
     }
 
     private fun getDoctorWithMinTimeStepIncrement(doctors: List<Doctor>): Doctor {
         return doctors.minBy { it.timeStep + it.consultationTimeMinute }
+    }
+
+    private fun getDoctorsWithMinTimeStep(timeStep: Int, doctors: List<Doctor>): List<Doctor> {
+        val doctorList = ArrayList<Doctor>()
+        doctors.map {
+            if (timeStep == it.timeStep) {
+                doctorList.add(it)
+            }
+        }
+
+        return doctorList
+    }
+
+    private fun getDoctorWithMinConsultationTime(doctors: List<Doctor>): Doctor {
+        return doctors.minBy { it.consultationTimeMinute }
     }
 
     private fun incrementDoctorTimeStep(doctor: Doctor) {
--- app/src/test/java/com/fwhyn/myapplication/util/other/NoRelationTest.kt
+++ app/src/test/java/com/fwhyn/myapplication/util/other/NoRelationTest.kt
@@ -7,6 +7,8 @@ import org.junit.Test
 
 class NoRelationTest {
     private lateinit var noRelation: NoRelation
+
+    private lateinit var doctors0: List<Doctor>
     private lateinit var doctors1: List<Doctor>
     private lateinit var doctors2: List<Doctor>
     private lateinit var doctors3: List<Doctor>
@@ -24,6 +26,7 @@ class NoRelationTest {
 
         val doctorE = Doctor(4, 2)
 
+        doctors0 = listOf()
         doctors1 = listOf(doctorA)
         doctors2 = listOf(doctorA, doctorB)
         doctors3 = listOf(doctorA, doctorB, doctorC)
@@ -44,67 +47,80 @@ class NoRelationTest {
     // doctor E: 2, 6, 10, 14, 18, 22, 26, 30, 34
 
     @Test
-    fun getTimeTest4Doctors() {
+    fun getConsultationTimeTest4Doctors() {
         // 0, 0, 0, 0, 2, 3, 4, 4, 6, 6, 8, 8, 9, 10, 12, 12, 12, 13, 14, 15, 16
-        Assert.assertEquals(0, noRelation.getConsultationTime(doctors4, 4))
-        Assert.assertEquals(2, noRelation.getConsultationTime(doctors4, 5))
-        Assert.assertEquals(3, noRelation.getConsultationTime(doctors4, 6))
-        Assert.assertEquals(4, noRelation.getConsultationTime(doctors4, 7))
-        Assert.assertEquals(4, noRelation.getConsultationTime(doctors4, 8))
-        Assert.assertEquals(6, noRelation.getConsultationTime(doctors4, 9))
-        Assert.assertEquals(6, noRelation.getConsultationTime(doctors4, 10))
-        Assert.assertEquals(8, noRelation.getConsultationTime(doctors4, 11))
-        Assert.assertEquals(10, noRelation.getConsultationTime(doctors4, 14))
-        Assert.assertEquals(12, noRelation.getConsultationTime(doctors4, 17))
-        Assert.assertEquals(13, noRelation.getConsultationTime(doctors4, 18))
-        Assert.assertEquals(14, noRelation.getConsultationTime(doctors4, 19))
+        Assert.assertEquals(0, noRelation.getConsultationTimeOrNull(doctors4, 4))
+        Assert.assertEquals(2, noRelation.getConsultationTimeOrNull(doctors4, 5))
+        Assert.assertEquals(3, noRelation.getConsultationTimeOrNull(doctors4, 6))
+        Assert.assertEquals(4, noRelation.getConsultationTimeOrNull(doctors4, 7))
+        Assert.assertEquals(4, noRelation.getConsultationTimeOrNull(doctors4, 8))
+        Assert.assertEquals(6, noRelation.getConsultationTimeOrNull(doctors4, 9))
+        Assert.assertEquals(6, noRelation.getConsultationTimeOrNull(doctors4, 10))
+        Assert.assertEquals(8, noRelation.getConsultationTimeOrNull(doctors4, 11))
+        Assert.assertEquals(10, noRelation.getConsultationTimeOrNull(doctors4, 14))
+        Assert.assertEquals(12, noRelation.getConsultationTimeOrNull(doctors4, 17))
+        Assert.assertEquals(13, noRelation.getConsultationTimeOrNull(doctors4, 18))
+        Assert.assertEquals(14, noRelation.getConsultationTimeOrNull(doctors4, 19))
     }
 
     @Test
-    fun getTimeTest3Doctors() {
+    fun getConsultationTimeTest3Doctors() {
         // doctor > patient -> expected 0
-        Assert.assertEquals(0, noRelation.getConsultationTime(doctors3, 1))
-        Assert.assertEquals(0, noRelation.getConsultationTime(doctors3, 2))
+        Assert.assertEquals(0, noRelation.getConsultationTimeOrNull(doctors3, 1))
+        Assert.assertEquals(0, noRelation.getConsultationTimeOrNull(doctors3, 2))
 
         // doctor = patient -> expected 0
-        Assert.assertEquals(0, noRelation.getConsultationTime(doctors3, 3))
+        Assert.assertEquals(0, noRelation.getConsultationTimeOrNull(doctors3, 3))
 
         // doctor + 1 = patient
-        Assert.assertEquals(2, noRelation.getConsultationTime(doctors3, 4))
+        Assert.assertEquals(2, noRelation.getConsultationTimeOrNull(doctors3, 4))
 
         // doctor > patient
         // 0, 0, 0, 2, 3, 4, 4, 6, 6, 8, 9, 12
-        Assert.assertEquals(3, noRelation.getConsultationTime(doctors3, 5))
-        Assert.assertEquals(6, noRelation.getConsultationTime(doctors3, 8))
-        Assert.assertEquals(6, noRelation.getConsultationTime(doctors3, 9))
+        Assert.assertEquals(3, noRelation.getConsultationTimeOrNull(doctors3, 5))
+        Assert.assertEquals(6, noRelation.getConsultationTimeOrNull(doctors3, 8))
+        Assert.assertEquals(6, noRelation.getConsultationTimeOrNull(doctors3, 9))
     }
 
     @Test
-    fun getTimeTest2Doctors() {
+    fun getConsultationTimeTest2Doctors() {
         // 11 patients: 0, 0, 3, 4, 6, 8, 9, 12, 12, 15, 16
-        Assert.assertEquals(16, noRelation.getConsultationTime(doctors2, 11))
+        Assert.assertEquals(16, noRelation.getConsultationTimeOrNull(doctors2, 11))
 
         // 15 patients: 0, 0, 3, 4, 6, 8, 9, 12, 12, 15, 16, 18, 20, 21, 24
-        Assert.assertEquals(24, noRelation.getConsultationTime(doctors2, 15))
+        Assert.assertEquals(24, noRelation.getConsultationTimeOrNull(doctors2, 15))
 
         // 26 patients: 0, 0, 3, 4, 6, 8, 9, 12, 12, 15, 16, 18, 20, 21, 24, 24, 27, 28, 30, 32,
         // 33, 36, 36, 38, 39, 42
     }
 
     @Test
-    fun getTimeTest1Doctor() {
+    fun getConsultationTimeTest1Doctor() {
         // 0, 3, 6, 9, 12, 15, 18, 21
-        Assert.assertEquals(15, noRelation.getConsultationTime(doctors1, 6))
-        Assert.assertEquals(18, noRelation.getConsultationTime(doctors1, 7))
+        Assert.assertEquals(0, noRelation.getConsultationTimeOrNull(doctors1, 1))
+        Assert.assertEquals(15, noRelation.getConsultationTimeOrNull(doctors1, 6))
+        Assert.assertEquals(18, noRelation.getConsultationTimeOrNull(doctors1, 7))
     }
 
     @Test
-    fun getTimeTest2DoctorsWithInitTime() {
+    fun getConsultationTimeTest2DoctorsWithInitTime() {
         // 0, 2, 3, 6, 6, 9, 10, 12, 14, 15, 18, 18, 21, 22, 24, 26, 27, 30, 30, 33, 34
-        Assert.assertEquals(2, noRelation.getConsultationTime(doctors2i, 2))
-        Assert.assertEquals(6, noRelation.getConsultationTime(doctors2i, 5))
-        Assert.assertEquals(9, noRelation.getConsultationTime(doctors2i, 6))
-        Assert.assertEquals(18, noRelation.getConsultationTime(doctors2i, 11))
-        Assert.assertEquals(18, noRelation.getConsultationTime(doctors2i, 12))
+        Assert.assertEquals(2, noRelation.getConsultationTimeOrNull(doctors2i, 2))
+        Assert.assertEquals(6, noRelation.getConsultationTimeOrNull(doctors2i, 5))
+        Assert.assertEquals(9, noRelation.getConsultationTimeOrNull(doctors2i, 6))
+        Assert.assertEquals(18, noRelation.getConsultationTimeOrNull(doctors2i, 11))
+        Assert.assertEquals(18, noRelation.getConsultationTimeOrNull(doctors2i, 12))
+    }
+
+    @Test
+    fun getConsultationTimeTestNoPatientReturnsNull() {
+        Assert.assertNull(noRelation.getConsultationTimeOrNull(doctors1, 0))
+        Assert.assertNull(noRelation.getConsultationTimeOrNull(doctors2, 0))
+    }
+
+    @Test
+    fun getConsultationTimeTestNoDoctorReturnsNull() {
+        Assert.assertNull(noRelation.getConsultationTimeOrNull(doctors0, 0))
+        Assert.assertNull(noRelation.getConsultationTimeOrNull(doctors0, 1))
     }
 }

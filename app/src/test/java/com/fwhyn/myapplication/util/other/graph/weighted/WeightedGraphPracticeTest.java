1bdb26a81dc011436a097412d031f349de919137
fwhyn <fwhynseventh@gmail.com>
2023-09-03 22:01:24 +0700

    Weighted Graph

--- app/src/test/java/com/fwhyn/myapplication/util/other/graph/weighted/WeightedGraphPracticeTest.java
+++ app/src/test/java/com/fwhyn/myapplication/util/other/graph/weighted/WeightedGraphPracticeTest.java
@@ -63,18 +63,91 @@ public class WeightedGraphPracticeTest {
         Assert.assertEquals(3, graph.getShortestPath(adj, source, destination));
     }
 
-//    @Test
-//    public void mainTest4() {
-//        WeightedGraphPractice.Edge adj = new WeightedGraphPractice.Edge();
-//
-//        adj.addUni("A", "B", 2);
-//        adj.addUni("B", "C", 1);
-//        adj.addUni("B", "E", 6);
-//
-//
-//        String source = "C";
-//        String destination = "E";
-//
-//        Assert.assertEquals(3, graph.getShortestPath(adj, source, destination));
-//    }
+    @Test
+    public void mainTest4() {
+        WeightedGraphPractice.Edge adj = new WeightedGraphPractice.Edge();
+
+        adj.addUni("A", "B", 2);
+        adj.addUni("B", "C", 1);
+        adj.addUni("B", "E", 6);
+        adj.addUni("C", "D", 3);
+        adj.addUni("D", "B", 10);
+        adj.addUni("E", "F", 4);
+        adj.addUni("E", "G", 3);
+        adj.addUni("F", "B", 5);
+        adj.addUni("F", "A", 3);
+        adj.addUni("G", "H", 1);
+
+
+        String source = "C";
+        String destination = "E";
+
+        Assert.assertEquals(19, graph.getShortestPath(adj, source, destination));
+    }
+
+    @Test
+    public void mainTest5() {
+        WeightedGraphPractice.Edge adj = new WeightedGraphPractice.Edge();
+
+        adj.addUni("A", "B", 2);
+        adj.addUni("B", "C", 1);
+        adj.addUni("B", "E", 6);
+        adj.addUni("C", "D", 3);
+        adj.addUni("D", "B", 10);
+        adj.addUni("E", "F", 4);
+        adj.addUni("E", "G", 3);
+        adj.addUni("F", "B", 5);
+        adj.addUni("F", "A", 3);
+        adj.addUni("G", "H", 1);
+
+
+        String source = "H";
+        String destination = "G";
+
+        Assert.assertEquals(2147483647, graph.getShortestPath(adj, source, destination));
+    }
+
+    @Test
+    public void mainTest6() {
+        WeightedGraphPractice.Edge adj = new WeightedGraphPractice.Edge();
+
+        adj.addUni("A", "B", 2);
+        adj.addUni("B", "C", 1);
+        adj.addUni("B", "E", 6);
+        adj.addUni("C", "D", 3);
+        adj.addUni("D", "B", 10);
+        adj.addUni("E", "F", 4);
+        adj.addUni("E", "G", 3);
+        adj.addUni("F", "B", 5);
+        adj.addUni("F", "A", 3);
+        adj.addUni("G", "H", 1);
+
+
+        String source = "E";
+        String destination = "B";
+
+        Assert.assertEquals(9, graph.getShortestPath(adj, source, destination));
+    }
+
+    @Test
+    public void mainTest7() {
+        WeightedGraphPractice.Edge adj = new WeightedGraphPractice.Edge();
+
+        adj.addUni("A", "B", 2);
+        adj.addUni("B", "C", 1);
+        adj.addUni("B", "E", 6);
+        adj.addUni("C", "D", 3);
+        adj.addUni("D", "B", 10);
+        adj.addUni("E", "F", 4);
+        adj.addUni("E", "G", 3);
+        adj.addUni("F", "B", 5);
+        adj.addUni("F", "A", 3);
+        adj.addUni("G", "H", 1);
+
+
+        String source = "E";
+        String destination = "D";
+
+        Assert.assertEquals(13, graph.getShortestPath(adj, source, destination));
+    }
 }

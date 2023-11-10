c9e7c1e51f52a1b79e4ffc3e49df804e94aaf075
Yana Wahyuna <yana.wahyuna@epson.co.id>
2023-11-10 10:25:08 +0700

    Hands-on migration
    added implementation

--- view/src/main/java/com/fwhyn/view/sunflower/plantdetail/PlantDetailFragment.kt
+++ view/src/main/java/com/fwhyn/view/sunflower/plantdetail/PlantDetailFragment.kt
@@ -21,6 +21,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material3.MaterialTheme
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.app.ShareCompat
 import androidx.core.widget.NestedScrollView
@@ -106,7 +107,15 @@ class PlantDetailFragment : Fragment() {
                         createShareIntent()
                         true
                     }
+
                     else -> false
+                }
+            }
+
+            composeView.setContent {
+                // You're in Compose world!
+                MaterialTheme {
+                    PlantDetailDescription()
                 }
             }
         }

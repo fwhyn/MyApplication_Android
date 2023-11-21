f17eeac92521e09ac2435346e129b32611ad0ca8
Yana Wahyuna <yana.wahyuna@epson.co.id>
2023-11-21 14:07:24 +0700

    ML Kit
    added implementation

---
+++ mlkit/src/main/java/com/fwhyn/mlkit/util/Extension.kt
@@ -0,0 +1,19 @@
+package com.fwhyn.mlkit.util
+
+import android.content.Context
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import java.util.concurrent.Executor
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+val Context.executor: Executor
+    get() = ContextCompat.getMainExecutor(this)
+
+suspend fun Context.getCameraProvider(): ProcessCameraProvider = suspendCoroutine { continuation ->
+    ProcessCameraProvider.getInstance(this).also { future ->
+        future.addListener({
+            continuation.resume(future.get())
+        }, executor)
+    }
+}

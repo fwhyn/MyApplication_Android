68000370f553ae0b33cea257dfe36ecf0f6c66c3
fwhyn <fwhynseventh@gmail.com>
2023-11-29 22:14:50 +0700

    BLE
    added implementation

--- connectivity/src/main/java/com/fwhyn/connectivity/ConnectivityActivity.kt
+++ connectivity/src/main/java/com/fwhyn/connectivity/ConnectivityActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.fwhyn.connectivity.ble.BleScan
 import com.fwhyn.connectivity.ble.BluetoothCheck
 import com.fwhyn.connectivity.ble.BluetoothCheckCallback
 
 class ConnectivityActivity : AppCompatActivity() {
     private val bluetoothCheck = BluetoothCheck(this, object : BluetoothCheckCallback {
         override fun ableToScan() {
-            Toast.makeText(this@ConnectivityActivity, "Ok", Toast.LENGTH_SHORT).show()
+            // Toast.makeText(this@ConnectivityActivity, "Ok", Toast.LENGTH_SHORT).show()
+            BleScan(this@ConnectivityActivity).scanLeDevice(true)
         }
 
         override fun unableToScan(reason: BluetoothCheckCallback.Reason) {
